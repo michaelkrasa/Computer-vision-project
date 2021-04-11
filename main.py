ebf98158e0674ae06f26cbd050cdf404d089cf84
Michael Krasa <michael.krasa@gmail.com>
2021-04-11 15:39:33 +0200

    Finished task 2

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-# Open JPEG image
-img = cv.imread('kitty.bmp', cv.COLOR_BGR2GRAY)
-
 
 def convolve_3x3(image, kernel):
     (imgHeight, imgWidth) = image.shape[:2]
@@ -26,6 +23,9 @@ def convolve_3x3(image, kernel):
     return output
 
 
+# Open JPEG image
+img = cv.imread('kitty.bmp', cv.COLOR_BGR2GRAY)
+
 # Initialising kernels
 kernel_ones = np.ones((3, 3), np.float32) / 9
 
@@ -33,6 +33,13 @@ kernel_weighted = np.ones((3, 3), np.float32)
 kernel_weighted[1][1] = 10  # weighted kernel center
 kernel_weighted /= 18
 
-# Save image in PNG format
+# Task 1
 cv.imwrite('kitty3x3.bmp', convolve_3x3(img, kernel_ones))  # 2D convolution
 cv.imwrite('kitty3x3-weighted.bmp', convolve_3x3(img, kernel_weighted))
+
+# Task 2
+sobel_x = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=3)
+sobel_y = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=3)
+cv.imwrite('histogram_x.bmp', sobel_x)
+cv.imwrite('histogram_y.bmp', sobel_y)
+cv.imwrite('histogram_combined.bmp', cv.add(sobel_x, sobel_y))  # edge strength image
